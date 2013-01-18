75e452070ceba5d16b22ee8f592b5322255c1aaf
Josh Benner <sjbenner@gmail.com>
2013-01-18 06:56:17 -0800

    changed comments; refactored isNullOrEmpty

--- lab2/Src/Util/StringUtil.php
+++ lab2/Src/Util/StringUtil.php
@@ -8,11 +8,11 @@ class StringUtil
      * @brief return whether string input is null or empty
      */
     public static function isNullOrEmpty( $value ) {
-        if( is_array( $value ) ) {
+        if( is_array( $value ) && 0 === count( $value ) ) {
             throw new \InvalidArgumentException( 'Args must be strings.' );
         }
 
-        if( !isset($value) || empty($value) ) {
+        if( null === $value || "" === $value ) {
             return true;
         }
 
--- lab2/Tests/Util/StringUtilTest.php
+++ lab2/Tests/Util/StringUtilTest.php
@@ -3,7 +3,6 @@ require "Vendors/autoload.php";
 
 class testIsEmptyOrNull extends PHPUnit_Framework_TestCase
 {
-
     /*
      * test if string is null or empty
      */
