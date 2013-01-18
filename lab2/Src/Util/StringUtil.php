170774a230314dd28971101ee1d458fedb3a621c
Josh Benner <sjbenner@gmail.com>
2013-01-17 20:14:39 -0800

    added test cases

--- lab2/Src/Util/StringUtil.php
+++ lab2/Src/Util/StringUtil.php
@@ -8,6 +8,11 @@ class StringUtil
      * @brief return whether string input is null or empty
      */
     public static function isNullOrEmpty( $value ) {
+        if( !isset($value) || empty($value) ) {
+            return true;
+        }
+
+        return false;
     }
 }
 ?>
--- lab2/Tests/Util/StringUtilTest.php
+++ lab2/Tests/Util/StringUtilTest.php
@@ -4,7 +4,11 @@ require "Vendors/autoload.php";
 class testIsEmptyOrNull extends PHPUnit_Framework_TestCase
 {
     public function testEmpty() {
-        $this->assertTrue( StringUtil::isNullOrEmpty("") );
+        $this->assertTrue( StringUtil::isNullOrEmpty( null ) );
+        $this->assertTrue( StringUtil::isNullOrEmpty( '' ) );
+        $this->assertFalse( StringUtil::isNullOrEmpty( 'abc' ) );
+        $this->assertFalse( StringUtil::isNullOrEmpty( 1234 ) );
+        $this->assertFalse( StringUtil::isNullOrEmpty( array() ) );
     }
 }
 ?>
