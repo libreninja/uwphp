018f3ade71553627dcc1674010a8ddd4ce770815
Josh Benner <cloudclear@ymail.com>
2013-01-27 16:17:21 -0800

    fixed file doc block

--- assignment1_2/Tests/Src/VehicleTest.php
+++ assignment1_2/Tests/Src/VehicleTest.php
@@ -1,4 +1,12 @@
 <?php 
+/**
+ * Assignment 1 part 2 - uw-php-course
+ *
+ * @author Josh Benner
+ * @version 0.1.2
+ * @copyright Josh Benner, 26 January, 2013
+ * @package default
+ **/
 namespace Tests\Src;
 
 
